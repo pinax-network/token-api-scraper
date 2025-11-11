19c41ba57ec454f13711c697255ccc73a904e070
Denis Carriere <carriere.denis@gmail.com>
2025-11-11 15:11:40 -0500

    update sql

--- sql/get_latest_transfers.sql
+++ sql/get_latest_transfers.sql
@@ -1,5 +1,11 @@
+-- don't include contract/account that already exists in `trc20_balances_rpc`
+WITH balances AS (
+    SELECT DISTINCT contract, account
+    FROM trc20_balances_rpc
+)
 SELECT DISTINCT
     log_address, `from`, `to`
 FROM trc20_transfer
+WHERE (log_address, `to`) NOT IN balances
 ORDER BY timestamp DESC
-LIMIT 1000000;
+LIMIT 10000;
