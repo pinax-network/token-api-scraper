e92a0e42a8475083f92afb4023f97150cabb7cd3
Denis Carriere <carriere.denis@gmail.com>
2025-12-17 09:18:43 -0500

    update native contracts

--- sql.schemas/schema.metadata.sql
+++ sql.schemas/schema.metadata.sql
@@ -31,30 +31,12 @@ ORDER BY (
 
 -- base,avalanche,unichain,tron,bsc,polygon,mainnet,arbitrum-one,optimism
 INSERT INTO metadata (network, contract, decimals, name, symbol) VALUES
-    ('tron', 'T0000000000000000000000000000000000000001', 6, 'Tron', 'TRX'),
     ('tron', 'T9yD14Nj9j7xAB4dbGeiX9h8unkKHxuWwb', 6, 'Tron', 'TRX'),
-    ('tron', '', 6, 'Tron', 'TRX'),
-    ('mainnet', '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 18, 'Ethereum', 'ETH'),
     ('mainnet', '0x0000000000000000000000000000000000000000', 18, 'Ethereum', 'ETH'),
-    ('mainnet', '', 18, 'Ethereum', 'ETH'),
-    ('bsc', '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 18, 'BNB', 'BNB'),
     ('bsc', '0x0000000000000000000000000000000000000000', 18, 'BNB', 'BNB'),
-    ('bsc', '', 18, 'BNB', 'BNB'),
-    ('polygon', '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 18, 'MATIC', 'MATIC'),
     ('polygon', '0x0000000000000000000000000000000000000000', 18, 'MATIC', 'MATIC'),
-    ('polygon', '', 18, 'MATIC', 'MATIC'),
-    ('avalanche', '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 18, 'AVAX', 'AVAX'),
     ('avalanche', '0x0000000000000000000000000000000000000000', 18, 'AVAX', 'AVAX'),
-    ('avalanche', '', 18, 'AVAX', 'AVAX'),
-    ('optimism', '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 18, 'ETH', 'ETH'),
     ('optimism', '0x0000000000000000000000000000000000000000', 18, 'ETH', 'ETH'),
-    ('optimism', '', 18, 'ETH', 'ETH'),
-    ('arbitrum-one', '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 18, 'ETH', 'ETH'),
     ('arbitrum-one', '0x0000000000000000000000000000000000000000', 18, 'ETH', 'ETH'),
-    ('arbitrum-one', '', 18, 'ETH', 'ETH'),
-    ('unichain', '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 18, 'UNI', 'UNI'),
-    ('unichain', '0x0000000000000000000000000000000000000000', 18, 'UNI', 'UNI'),
-    ('unichain', '', 18, 'UNI', 'UNI'),
-    ('base', '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', 18, 'ETH', 'ETH'),
-    ('base', '0x0000000000000000000000000000000000000000', 18, 'ETH', 'ETH'),
-    ('base', '', 18, 'ETH', 'ETH');
+    ('unichain', '0x0000000000000000000000000000000000000000', 18, 'ETH', 'ETH'),
+    ('base', '0x0000000000000000000000000000000000000000', 18, 'ETH', 'ETH');
