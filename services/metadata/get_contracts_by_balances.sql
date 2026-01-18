da31b4c39deb835a46d323e04ef9be0c65d24ac7
Denis Carriere <carriere.denis@gmail.com>
2026-01-17 22:03:37 -0500

    change to erc20

--- services/metadata/get_contracts_by_balances.sql
+++ services/metadata/get_contracts_by_balances.sql
@@ -2,7 +2,7 @@ SELECT
     contract,
     max(block_num) as block_num,
     max(timestamp) as timestamp
-FROM balances
+FROM erc20_balances
 WHERE
     contract NOT IN (SELECT contract FROM metadata_errors) AND
     contract NOT IN (SELECT contract FROM metadata)
