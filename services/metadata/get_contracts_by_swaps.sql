935e1f4d87cbd24fa1925829412fe5c0f804de6e
Denis Carriere <carriere.denis@gmail.com>
2026-01-19 15:43:33 -0500

    update network with ANTI JOIN

--- services/metadata/get_contracts_by_swaps.sql
+++ services/metadata/get_contracts_by_swaps.sql
@@ -4,16 +4,14 @@ WITH contracts AS (
         max(block_num) as block_num,
         max(timestamp) as timestamp
     FROM swaps
-    WHERE network = {network: String}
     GROUP BY contract
 )
 SELECT
-    contract,
+    c.contract as contract,
     c.block_num as block_num,
     c.timestamp as timestamp
 FROM contracts c
-LEFT JOIN (SELECT contract FROM metadata_errors WHERE network = {network: String}) me ON c.contract = me.contract
-LEFT JOIN (SELECT contract FROM metadata WHERE network = {network: String}) m ON c.contract = m.contract
-WHERE me.contract IS NULL AND m.contract IS NULL
+ANTI LEFT JOIN (SELECT contract FROM metadata_errors WHERE network = {network: String}) me ON c.contract = me.contract
+ANTI LEFT JOIN (SELECT contract FROM metadata WHERE network = {network: String}) m ON c.contract = m.contract
 ORDER BY c.timestamp DESC
 LIMIT 1000000;
