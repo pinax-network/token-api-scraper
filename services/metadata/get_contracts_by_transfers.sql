3d96c5cff99e7718e60af26cb97d3a813d0223d8
Denis Carriere <carriere.denis@gmail.com>
2026-01-18 21:33:37 -0500

    update transfers

--- services/metadata/get_contracts_by_transfers.sql
+++ services/metadata/get_contracts_by_transfers.sql
@@ -1,11 +1,17 @@
+WITH all_transfers AS (
+    SELECT
+        log_address AS contract,
+        max(block_num) as block_num,
+        max(timestamp) as timestamp
+    FROM transfers
+    GROUP BY contract
+)
 SELECT
-    log_address AS contract,
-    max(block_num) as block_num,
-    max(timestamp) as timestamp
-FROM transfers
-WHERE
-    contract NOT IN (SELECT contract FROM metadata_errors) AND
-    contract NOT IN (SELECT contract FROM metadata)
-GROUP BY contract
-ORDER BY timestamp DESC
+    contract,
+    t.block_num as block_num,
+    t.timestamp as timestamp
+FROM all_transfers t
+ANTI LEFT JOIN metadata_errors USING (contract)
+ANTI LEFT JOIN metadata USING (contract)
+ORDER BY t.timestamp DESC
 LIMIT 1000000;
